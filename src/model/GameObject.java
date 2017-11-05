ca12529fc775a94cf22795d421ebf2606a3f9f71
darknight060198 <darkiller060198.3@gmail.com>
2017-11-05 23:50:51 +0700

    Update untuk kelas GameObject. Ditambahkan setter dan getter yang diperlukan. Ditambahkan pula method untuk menginisialisasi AnimationController dan AnimationChannel.

--- src/model/GameObject.java
+++ src/model/GameObject.java
@@ -17,6 +17,7 @@ import com.jme3.material.Material;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 import com.jme3.scene.shape.Box;
@@ -34,22 +35,84 @@ import java.io.IOException;
  */
 public abstract class GameObject extends AbstractControl implements AnimEventListener {
 
-    public static final byte BOX = 1;
-    public static final byte CYLINDER = 2;
-    public static final byte QUAD = 3;
-    public static final byte SPHERE = 4;
-
+    /**
+     *
+     */
     protected float x;
+
+    /**
+     *
+     */
     protected float y;
+
+    /**
+     *
+     */
     protected float z;
+
+    /**
+     *
+     */
     protected float vx;
+
+    /**
+     *
+     */
     protected float vy;
+
+    /**
+     *
+     */
     protected float vz;
+
+    /**
+     *
+     */
     protected float width;
+
+    /**
+     *
+     */
     protected float height;
+
+    /**
+     *
+     */
     protected float length;
+
+    /**
+     *
+     */
     protected String name;
 
+    /**
+     *
+     */
+    protected AnimChannel animChannel;
+
+    /**
+     *
+     */
+    protected AnimControl animControl;
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param z
+     * @param vx
+     * @param vy
+     * @param vz
+     * @param width
+     * @param height
+     * @param length
+     * @param type
+     * @param closedOrFlipped
+     * @param name
+     * @param am
+     * @param mat
+     * @param texture
+     */
     public GameObject(float x, float y, float z, float vx, float vy, float vz,
             float width, float height, float length, byte type, boolean closedOrFlipped,
             String name, AssetManager am, Material mat, Texture texture) {
@@ -64,22 +127,22 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         this.name = name;
 
         switch (type) {
-            case BOX:
+            case GameUtil.BOX:
                 Box box = new Box(length / 2.0f, height / 2.0f, width / 2.0f);
                 this.spatial = new Geometry(name, box);
                 this.spatial.setLocalTranslation(x, y, z);
                 break;
-            case CYLINDER:
+            case GameUtil.CYLINDER:
                 Cylinder cylinder = new Cylinder(100, 100, length, width, closedOrFlipped);
                 this.spatial = new Geometry(name, cylinder);
                 this.spatial.setLocalTranslation(x, y, z);
                 break;
-            case QUAD:
+            case GameUtil.QUAD:
                 Quad quad = new Quad(length, height);
                 this.spatial = new Geometry(name, quad);
                 this.spatial.setLocalTranslation(x - length / 2, y - height / 2, z);
                 break;
-            case SPHERE:
+            case GameUtil.SPHERE:
                 Sphere sphere = new Sphere(50, 50, length);
                 this.spatial = new Geometry(name, sphere);
                 this.spatial.setLocalTranslation(x, y, z);
@@ -87,8 +150,25 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         }
         mat.setTexture(name, texture);
         this.spatial.setMaterial(mat);
+        
+        setUpAnim();
     }
-    
+
+    /**
+     *
+     * @param vx
+     * @param vy
+     * @param vz
+     * @param width
+     * @param height
+     * @param length
+     * @param type
+     * @param closedOrFlipped
+     * @param name
+     * @param am
+     * @param mat
+     * @param texture
+     */
     public GameObject(float vx, float vy, float vz,
             float width, float height, float length, byte type, boolean closedOrFlipped,
             String name, AssetManager am, Material mat, Texture texture) {
@@ -103,27 +183,41 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         this.name = name;
 
         switch (type) {
-            case BOX:
+            case GameUtil.BOX:
                 Box box = new Box(length / 2.0f, height / 2.0f, width / 2.0f);
                 this.spatial = new Geometry(name, box);
                 break;
-            case CYLINDER:
+            case GameUtil.CYLINDER:
                 Cylinder cylinder = new Cylinder(100, 100, length, width, closedOrFlipped);
                 this.spatial = new Geometry(name, cylinder);
                 break;
-            case QUAD:
+            case GameUtil.QUAD:
                 Quad quad = new Quad(length, height);
                 this.spatial = new Geometry(name, quad);
                 break;
-            case SPHERE:
+            case GameUtil.SPHERE:
                 Sphere sphere = new Sphere(50, 50, length);
                 this.spatial = new Geometry(name, sphere);
                 break;
         }
         mat.setTexture(name, texture);
         this.spatial.setMaterial(mat);
+        
+        setUpAnim();
     }
-    
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @param length
+     * @param type
+     * @param closedOrFlipped
+     * @param name
+     * @param am
+     * @param mat
+     * @param texture
+     */
     public GameObject(float width, float height, float length, byte type, boolean closedOrFlipped,
             String name, AssetManager am, Material mat, Texture texture) {
         this.x = 0;
@@ -137,27 +231,41 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         this.name = name;
 
         switch (type) {
-            case BOX:
+            case GameUtil.BOX:
                 Box box = new Box(length / 2.0f, height / 2.0f, width / 2.0f);
                 this.spatial = new Geometry(name, box);
                 break;
-            case CYLINDER:
+            case GameUtil.CYLINDER:
                 Cylinder cylinder = new Cylinder(100, 100, length, width, closedOrFlipped);
                 this.spatial = new Geometry(name, cylinder);
                 break;
-            case QUAD:
+            case GameUtil.QUAD:
                 Quad quad = new Quad(length, height);
                 this.spatial = new Geometry(name, quad);
                 break;
-            case SPHERE:
+            case GameUtil.SPHERE:
                 Sphere sphere = new Sphere(50, 50, length);
                 this.spatial = new Geometry(name, sphere);
                 break;
         }
         mat.setTexture(name, texture);
         this.spatial.setMaterial(mat);
+        
+        setUpAnim();
     }
-    
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param z
+     * @param vx
+     * @param vy
+     * @param vz
+     * @param name
+     * @param path
+     * @param am
+     */
     public GameObject(float x, float y, float z, float vx, float vy, float vz,
             String name, String path, AssetManager am) {
         this.x = x;
@@ -171,8 +279,16 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         this.name = name;
 
         this.spatial = am.loadModel(path);
+        
+        setUpAnim();
     }
-    
+
+    /**
+     *
+     * @param name
+     * @param path
+     * @param am
+     */
     public GameObject(String name, String path, AssetManager am) {
         this.x = 0;
         this.y = 0;
@@ -185,20 +301,37 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         this.name = name;
 
         this.spatial = am.loadModel(path);
+        
+        setUpAnim();
     }
 
-    @Override
-    protected void controlUpdate(float tpf) {
-        x += vx * tpf;
-        y += vy * tpf;
-        z += vz * tpf;
-        this.spatial.setLocalTranslation(x, y, z);
+    /**
+     *
+     * @param name
+     */
+    public GameObject(String name) {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.vx = 0;
+        this.vy = 0;
+        this.width = 0;
+        this.height = 0;
+        this.length = 0;
+        this.name = name;
+        
+        setUpAnim();
     }
-
+    
+    /**
+     *
+     * @param ex
+     * @throws IOException
+     */
     @Override
     public void write(JmeExporter ex) throws IOException {
         super.write(ex);
-        
+
         OutputCapsule oc = ex.getCapsule(this);
         oc.write(y, "y", 0);
         oc.write(x, "x", 0);
@@ -212,10 +345,15 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         oc.write(name, "name", "");
     }
 
+    /**
+     *
+     * @param im
+     * @throws IOException
+     */
     @Override
     public void read(JmeImporter im) throws IOException {
         super.read(im);
-        
+
         InputCapsule ic = im.getCapsule(this);
         enabled = ic.readBoolean("enabled", true);
         spatial = (Spatial) ic.readSavable("spatial", null);
@@ -231,8 +369,161 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
         name = ic.readString("name", "");
     }
 
+    /**
+     *
+     * @param node
+     */
+    public void attachToNode(Node node) {
+        node.attachChild(this.spatial);
+    }
+
+    /**
+     *
+     * @param node
+     */
+    public void dettachFromNode(Node node) {
+        node.detachChild(this.spatial);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getX() {
+        return x;
+    }
+
+    /**
+     *
+     * @param x
+     */
+    public void setX(float x) {
+        this.x = x;
+        this.spatial.setLocalTranslation(x, y, z);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getY() {
+        return y;
+    }
+
+    /**
+     *
+     * @param y
+     */
+    public void setY(float y) {
+        this.y = y;
+        this.spatial.setLocalTranslation(x, y, z);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getZ() {
+        return z;
+    }
+
+    /**
+     *
+     * @param z
+     */
+    public void setZ(float z) {
+        this.z = z;
+        this.spatial.setLocalTranslation(x, y, z);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getVx() {
+        return vx;
+    }
+
+    /**
+     *
+     * @param vx
+     */
+    public void setVx(float vx) {
+        this.vx = vx;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getVy() {
+        return vy;
+    }
+
+    /**
+     *
+     * @param vy
+     */
+    public void setVy(float vy) {
+        this.vy = vy;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getVz() {
+        return vz;
+    }
+
+    /**
+     *
+     * @param vz
+     */
+    public void setVz(float vz) {
+        this.vz = vz;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getWidth() {
+        return width;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getHeight() {
+        return height;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public float getLength() {
+        return length;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    private void setUpAnim() {
+        animControl = this.spatial.getControl(AnimControl.class);
+        animControl.addListener(this);
+        animChannel = animControl.createChannel();
+    }
+
     /*
-     * TODO : To be implemented in subclass.
+     * To be implemented in subclass.
      */
     @Override
     protected abstract void controlRender(RenderManager rm, ViewPort vp);
@@ -242,6 +533,17 @@ public abstract class GameObject extends AbstractControl implements AnimEventLis
 
     @Override
     public abstract void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName);
+
+    @Override
+    protected abstract void controlUpdate(float tpf);
     
+    /**
+     *
+     */
+    public abstract void reset();
     
+    /**
+     *
+     */
+    public abstract void stop();
 }
