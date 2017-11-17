467fbb656eb7b9f3ffad2174b2429522ab548fc9
cantikaliem <31977075+cantikaliem@users.noreply.github.com>
2017-11-17 13:33:42 +0700

    Class Player
    class player sudah diperbaiki, change lane sudah dipindahkan ke controller sesuai kontrak kelas dan dokumentasi sudah lengkap

--- src/model/Player.java
+++ src/model/Player.java
@@ -13,23 +13,43 @@ import com.jme3.material.Material;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Spatial;
 import com.jme3.texture.Texture;
 import controller.ChangeLaneControl;
 import controller.MyCharacterControl;
 
 /**
- *
- * @author i16072
+ * This class is class for object player (character)
+ * @author i16012
  */
-public class Player extends GameObject implements ChangeLaneControl {
+public class Player extends GameObject {
 
     private static final int ROTATE_FRONT = -3;
 
-    private MyCharacterControl myCharCon;
-    private boolean jumping, changingLaneToLeft, changingLaneToRight;
-    private float tempRadians, vChangeLane, moveDistanceTemp;
-    private byte currentLane;
+    private MyCharacterControl myCharCon; //character's controller
+    private boolean jumping, //attribute for check whether the object is jumping 
+					changingLaneToLeft, //attribute for check whether the object is changing lane to left 
+					changingLaneToRight; //attribute for check whether the object is changing lane to right
+    private byte currentLane; //current lane of this object
 
+    /**
+     * Constructor
+     * @param x Coordinate x depends on the parent node
+     * @param y Coordinate y depends on the parent node
+     * @param z Coordinate z depends on the parent node
+     * @param vx The speed of this object is in the x axis
+     * @param vy The speed of this object is in the y axis
+     * @param vz The speed of this object is in the z axis
+     * @param width The width of this object
+     * @param height The height of this object
+     * @param length The length of this object
+     * @param type Geometric shape of this object
+     * @param closedOrFlipped condition for this object
+     * @param name The name for this object
+     * @param am Asset Manager for this object
+     * @param mat The material for this object
+     * @param texture The texture for this object
+     */
     public Player(float x, float y, float z, float vx, float vy, float vz, float width, float height, float length, byte type, boolean closedOrFlipped, String name, AssetManager am, Material mat, Texture texture) {
         super(x, y, z, vx, vy, vz, width, height, length, type, closedOrFlipped, name, am, mat, texture);
         setMyCharControl();
@@ -37,6 +57,21 @@ public class Player extends GameObject implements ChangeLaneControl {
         currentLane = 1;
     }
 
+    /**
+     * Constructor
+     * @param vx The speed of this object is in the x axis
+     * @param vy The speed of this object is in the y axis
+     * @param vz The speed of this object is in the z axis
+     * @param width The width of this object
+     * @param height The height of this object
+     * @param length The length of this object
+     * @param type Geometric shape of this object
+     * @param closedOrFlipped condition for this object
+     * @param name The name for this object
+     * @param am Asset Manager for this object
+     * @param mat The material for this object
+     * @param texture The texture for this object
+     */
     public Player(float vx, float vy, float vz, float width, float height, float length, byte type, boolean closedOrFlipped, String name, AssetManager am, Material mat, Texture texture) {
         super(vx, vy, vz, width, height, length, type, closedOrFlipped, name, am, mat, texture);
         setMyCharControl();
@@ -44,6 +79,18 @@ public class Player extends GameObject implements ChangeLaneControl {
         currentLane = 1;
     }
 
+    /**
+     * Constructor
+     * @param width The width of this object
+     * @param height The height of this object
+     * @param length The length of this object
+     * @param type Geometric shape of this object
+     * @param closedOrFlipped condition for this object
+     * @param name The name for this object
+     * @param am Asset Manager for this object
+     * @param mat The material for this object
+     * @param texture The texture for this object
+     */
     public Player(float width, float height, float length, byte type, boolean closedOrFlipped, String name, AssetManager am, Material mat, Texture texture) {
         super(width, height, length, type, closedOrFlipped, name, am, mat, texture);
         setMyCharControl();
@@ -51,6 +98,21 @@ public class Player extends GameObject implements ChangeLaneControl {
         currentLane = 1;
     }
 
+    /**
+     * Constructor
+     * @param x Coordinate x depends on the parent node
+     * @param y Coordinate y depends on the parent node
+     * @param z Coordinate z depends on the parent node
+     * @param vx The speed of this object is in the x axis
+     * @param vy The speed of this object is in the y axis
+     * @param vz The speed of this object is in the z axis
+     * @param width The width of this object
+     * @param height The height of this object
+     * @param length The length of this object
+     * @param name The name for this object
+     * @param path The path for this object's model
+     * @param am Asset Manager for this object
+     */
     public Player(float x, float y, float z, float vx, float vy, float vz, float width, float height, float length, String name, String path, AssetManager am) {
         super(x, y, z, vx, vy, vz, width, height, length, name, path, am);
         setMyCharControl();
@@ -58,6 +120,12 @@ public class Player extends GameObject implements ChangeLaneControl {
         currentLane = 1;
     }
 
+    /**
+     * Constructor
+     * @param name The name for this object
+     * @param path The path for this object's model
+     * @param am Asset Manager for this object
+     */
     public Player(String name, String path, AssetManager am) {
         super(name, path, am);
         setMyCharControl();
@@ -65,13 +133,20 @@ public class Player extends GameObject implements ChangeLaneControl {
         currentLane = 1;
     }
 
-    public Player(String name) {
-        super(name);
+    /**
+     * Constructor 
+     * @param obj player's object
+     */
+    public Player(Spatial obj) {
+        super(obj);
         setMyCharControl();
         myCharCon.setPlayer(this);
         currentLane = 1;
     }
 
+    /**
+     * Setter for character's controller
+     */
     private void setMyCharControl() {
         BoxCollisionShape boxShape = new BoxCollisionShape(new Vector3f(this.length / 2f, this.height / 2f, this.width / 2f));
         myCharCon = new MyCharacterControl(boxShape, 0.5f);
@@ -80,139 +155,126 @@ public class Player extends GameObject implements ChangeLaneControl {
         this.spatial.addControl(myCharCon);
     }
 
-    @Override
-    protected void controlRender(RenderManager rm, ViewPort vp) {
-
-    }
-
+    /**
+     * Method to update character's controller
+     * @param tpf time per frame
+     */
     @Override
     protected void controlUpdate(float tpf) {
-        if (jumping) {
-            myCharCon.update(tpf);
-            rotateToFrontAnimation(tpf);
-        } else {
-            normalRotationAnimation(tpf);
-        }
-
-        if (changingLaneToLeft || changingLaneToRight) {
-            vChangeLane -= tpf * GameUtil.ACCELERATOR_CHANGE_LANE;
-            if (changingLaneToLeft) {
-                moveDistanceTemp += tpf * vChangeLane;
-                x -= tpf * vChangeLane;
-                if (moveDistanceTemp > GameUtil.CHANGE_LANE_DISTANCE) {
-                    if (currentLane == GameUtil.CENTER_LANE) {
-                        x = GameUtil.CHANGE_LANE_DISTANCE * -1;
-                    } else if (currentLane == GameUtil.RIGHT_LANE) {
-                        x = 0;
-                    }
-                    moveDistanceTemp = 0;
-                    vChangeLane = 0;
-                    currentLane--;
-                    changingLaneToLeft = false;
-                    this.spatial.rotate(0, tempRadians * -1f, 0);
-                    tempRadians = 0;
-                }
-                rotateToLeftAnimation(tpf);
-            } else if (changingLaneToRight) {
-                moveDistanceTemp += tpf * vChangeLane;
-                x += tpf * vChangeLane;
-                if (moveDistanceTemp > GameUtil.CHANGE_LANE_DISTANCE) {
-                    if (currentLane == GameUtil.CENTER_LANE) {
-                        x = GameUtil.CHANGE_LANE_DISTANCE;
-                    } else if (currentLane == GameUtil.LEFT_LANE) {
-                        x = 0;
-                    }
-                    moveDistanceTemp = 0;
-                    currentLane++;
-                    vChangeLane = 0;
-                    changingLaneToRight = false;
-                    this.spatial.rotate(0, tempRadians * -1f, 0);
-                    tempRadians = 0;
-                }
-                rotateToRightAnimation(tpf);
-            }
-        }
-
+        myCharCon.update(tpf);
         this.spatial.setLocalTranslation(x, y, z);
     }
 
-    @Override
-    public void reset() {
-
-    }
-
-    @Override
-    public void stop() {
-
-    }
-
-    @Override
+    /**
+     * Method to change lane of the object
+     * @param lane the lane the player wants
+     */
     public void changeLane(byte lane) {
-        if (!changingLaneToLeft && !changingLaneToRight) {
-            if (lane == GameUtil.LEFT_LANE) {
-                if (currentLane == GameUtil.LEFT_LANE) {
-                    return;
-                }
-                changingLaneToLeft = true;
-            } else if (lane == GameUtil.RIGHT_LANE) {
-                if (currentLane == GameUtil.RIGHT_LANE) {
-                    return;
-                }
-                changingLaneToRight = true;
-            }
-
-            vChangeLane = GameUtil.V_CHANGE_LANE;
-        }
+        myCharCon.changeLane(lane);
     }
 
+    /**
+     * Method to make object jump
+     */
     public void jump() {
-        if (!jumping) {
-            myCharCon.jump();
-            jumping = true;
-        }
-    }
-
-    public void slide() {
-        //TODO : IMPLEMENT HERE LATER
+        myCharCon.jump();
     }
 
     /**
      * Animation to rotate the spatial to front when jumping
+     * @param tpf time per frame
      */
-    private void rotateToFrontAnimation(float tpf) {
+    public void rotateToFrontAnimation(float tpf) {
         this.spatial.rotate(tpf * ROTATE_FRONT * 5, 0, 0);
     }
 
     /**
      * Animation to rotate the spatial to left when changing lane
+     * @param tpf time per frame
      */
-    private void rotateToLeftAnimation(float tpf) {
+    public void rotateToLeftAnimation(float tpf) {
         this.spatial.rotate(0, tpf * GameUtil.ROTATE_SIDE * -1f, 0);
-        tempRadians += tpf * GameUtil.ROTATE_SIDE * -1f;
+//        tempRadians += tpf * GameUtil.ROTATE_SIDE * -1f;
     }
 
     /**
      * Animation to rotate the spatial to right when changing lane
+     * @param tpf time per frame
      */
-    private void rotateToRightAnimation(float tpf) {
+    public void rotateToRightAnimation(float tpf) {
         this.spatial.rotate(0, tpf * GameUtil.ROTATE_SIDE, 0);
-        tempRadians += tpf * GameUtil.ROTATE_SIDE;
+//        tempRadians += tpf * GameUtil.ROTATE_SIDE;
     }
 
-    private void normalRotationAnimation(float tpf) {
+    /**
+     * Animation to rotate the spatial to the front
+     * @param tpf time per frame
+     */
+    public void normalRotationAnimation(float tpf) {
         this.spatial.rotate(tpf * ROTATE_FRONT, 0, 0);
     }
 
     /**
-     *
-     * @return
+     * Method to check whether the object is jumping (getter)
+     * @return true if the object is jumping, false if the object isn't jumping
      */
     public boolean isJumping() {
         return jumping;
     }
 
+    /**
+     * Setter for variable jumping
+     * @param jumping new value of variable jumping
+     */
     public void setJumping(boolean jumping) {
         this.jumping = jumping;
     }
 
+    /**
+     * Method to check whether the object is changing lane to the left (getter)
+     * @return true if the object is changing lane to the left, false if the object isn't changing lane to the left
+     */
+    public boolean isChangingLaneToLeft() {
+        return changingLaneToLeft;
+    }
+
+    /**
+     * Setter for variable changingLaneToLeft
+     * @param changingLaneToLeft new value of variable changingLaneToLeft
+     */
+    public void setChangingLaneToLeft(boolean changingLaneToLeft) {
+        this.changingLaneToLeft = changingLaneToLeft;
+    }
+
+    /**
+     * Method to check whether the object is changing lane to the right (getter)
+     * @return true if the object is changing lane to the right, false if the object isn't changing lane to the right
+     */
+    public boolean isChangingLaneToRight() {
+        return changingLaneToRight;
+    }
+
+    /**
+     * Setter for variable changingLaneToRight
+     * @param changingLaneToRight new value of variable changingLaneToRight
+     */
+    public void setChangingLaneToRight(boolean changingLaneToRight) {
+        this.changingLaneToRight = changingLaneToRight;
+    }
+
+    /**
+     * Method to get current lane (getter)
+     * @return current lane         
+     */
+    public byte getCurrentLane() {
+        return currentLane;
+    }
+
+    /**
+     * Setter for variable currentLane
+     * @param currentLane new value of variable currentLane
+     */
+    public void setCurrentLane(byte currentLane) {
+        this.currentLane = currentLane;
+    }
 }
