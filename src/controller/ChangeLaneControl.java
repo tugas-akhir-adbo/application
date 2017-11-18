3fc9762e41179304f7763d2c911f391be30161b5
cantikaliem <31977075+cantikaliem@users.noreply.github.com>
2017-11-18 16:51:04 +0700

    controller
    ditambahkan interface level control untuk mengontrol level up dan restart level lengkap dengan dokumentasinya, dan dokumentasi semua kelas dilengkapi + dirapikan

--- src/controller/ChangeLaneControl.java
+++ src/controller/ChangeLaneControl.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 /**
- * Interface for change lane's controller
- * @author i16012
+ * This is interface for change lane's controller
+ * This interface has method for changing lane
+ * @author i16012 - Apsari Ayusya Cantika
+ * @since
+ * @version 
  */
 public interface ChangeLaneControl {
-    /**
+   /**
      * Method to control the character to change lane
      * @param lane the lane the player wants
      */
